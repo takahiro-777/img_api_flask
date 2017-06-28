65d6d30415397a7a8018389e8d3e81143ba2fcdf
Yutaka Tajika <y.tajika529@gmail.com>
2017-06-28 20:48:03 +0900

    make project

---
+++ iOS/Xcode8/ImageRecognizerSample01/ImageRecognizerSample01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageRecognizerSample01
+//
+//  Created by 多鹿豊 on 2017/06/28.
+//  Copyright © 2017年 Yutaka Tajika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOS/Xcode8/ImageRecognizerSample01/ImageRecognizerSample01Tests/ImageRecognizerSample01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ImageRecognizerSample01Tests.swift
+//  ImageRecognizerSample01Tests
+//
+//  Created by 多鹿豊 on 2017/06/28.
+//  Copyright © 2017年 Yutaka Tajika. All rights reserved.
+//
+
+import XCTest
+@testable import ImageRecognizerSample01
+
+class ImageRecognizerSample01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
